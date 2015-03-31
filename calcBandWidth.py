999dc56a18160a6767885ab8dc73357e92be78d1
Yifei Fu <yifei.fu@percolata.com>
2015-03-31 23:53:03 +0800

    trans

---
+++ calcBandWidth.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+#list of devices
+devList = []
+
+if __name__ == '__main__':
+
+    for each dev in devList:
+        1. calc worktime
+        2. prepare scripts for bandWidth testing
+        3. generate cron tasks
+    
+
+
+    in cron tasks:
+        calc bandWidth
+        record bandWidth of each Node
